a35557b38e676ed85c62284a31357cbc55c08494
System-in-a-System <textconstruction@potonmail.com>
2020-08-31 08:50:18 +0100

    Adding interation with the user in SwedishIDMain

--- src/SwedishIDMain.java
+++ src/SwedishIDMain.java
@@ -1,56 +1,38 @@
+import java.util.Scanner;
+
 public class SwedishIDMain {
 
 	public static void main(String[] args) {
 		
-		SwedishID id1 = new SwedishID("19270414-4506");
-		SwedishID id2 = new SwedishID("19270414-8671");
-		SwedishID id3 = new SwedishID("19801214-7727");
-		SwedishID id4 = new SwedishID("19811218-9876");
-		SwedishID id5 = new SwedishID("19770525-1111");
+		// Prompt user to enter an id number
+		System.out.print("Input an id (YYYYMMDD-XXXX): ");
 		
-		System.out.print(id1.showID() + " is a ");
-		if(id1.isFemale())
-			System.out.println("female.");
+		// Intake the input
+		Scanner input = new Scanner(System.in);
+		String inputID = input.nextLine();
+		
+		input.close();
+		
+		
+		// Instantiate a new SwedishID object
+		SwedishID id = new SwedishID(inputID);
+		
+		
+		
+		// Process and output the results		
+		System.out.print(id.showID() + " is a ");
+		if(id.isFemale())
+			System.out.println("female, " + id.calculateAge() + " years old");
 		else
-			System.out.println("male.");
+			System.out.println("male, " + id.calculateAge() + " years old");
 		
 		
-		System.out.print(id2.showID() + " is a ");
-		if(id2.isFemale())
-			System.out.println("female.");
+		
+		if(id.validID())
+			System.out.println(id.showID() + " is valid");
 		else
-			System.out.println("male.");
+			System.out.println(id.showID() + " is not valid");
 		
-		
-		
-		if(id1.comparedTo(id2) == 0)
-			System.out.println("Yes, " + id1.showID() + " and " + id2.showID() + " are the same.");
-		
-		if(id1.comparedTo(id3) < 0)
-			System.out.println(id1.showID() + " is older than " + id3.showID());
-		
-		if(id3.comparedTo(id1) > 0)
-			System.out.println(id3.showID() + " is younger than " + id1.showID());
-		
-		
-		
-		if(id1.validID())
-			System.out.println(id1.showID() + " is valid");
-		else
-			System.out.println(id1.showID() + " is not valid");
-		
-		
-		if(id4.validID())
-			System.out.println(id4.showID() + " is valid");
-		else
-			System.out.println(id4.showID() + " is not valid");
-		
-		
-		if(id5.validID())
-			System.out.println(id5.showID() + " is valid");
-		else
-			System.out.println(id5.showID() + " is not valid");
-
 	}
 
 }
